bc18d36f1d73d341441085cd6edf091a133ebb74
Samupro1234 <samuelcompi1011@gmail.com>
2023-01-30 21:47:16 +0100

    sam-actualizado

--- myproyecto4/blog/admin.py
+++ myproyecto4/blog/admin.py
@@ -1,6 +1,13 @@
 
 from django.contrib import admin
-from blog.models import usuario
-# Register your models here.
+from blog.models import usuario, visitante, guia, sitio
+# Register your models here.()min
 
-admin
+class usuarioAdmin(admin.ModelAdmin):
+    list_display = ('nombres', 'apellidos')
+
+admin.site.register(usuario,usuarioAdmin)
+admin.site.register(visitante)
+admin.site.register(guia)
+admin.site.register(sitio)
+
